b20fd22f2d13e28030e2d12253ea01b65d02fb26
Matt Oliver <protogonoi@gmail.com>
2020-04-15 04:53:22 +1000

    project: Update for 3.3 merge.

--- SMP/x265_config.h
+++ SMP/x265_config.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (C) 2013-2017 MulticoreWare, Inc
+ * Copyright (C) 2013-2020 MulticoreWare, Inc
  *
  * Authors: Steve Borho <steve@borho.org>
  *
@@ -29,6 +29,6 @@
 /* Incremented each time public API is changed, X265_BUILD is used as
  * the shared library SONAME on platforms which support it. It also
  * prevents linking against a different version of the static lib */
-#define X265_BUILD 179
+#define X265_BUILD 188
 
 #endif
